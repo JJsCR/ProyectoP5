9b59d7b776fdae24909443d49e587cd2271903a2
Kevin Hdz <kevinhdz27@hotmail.com>
2019-04-01 16:39:56 -0600

    Son las tres tareas

---
+++ BancoCentral/BancoCentral/Scripts/Grafico3.js
@@ -0,0 +1,31 @@
+﻿var TCDCanvas = document.getElementById("TCDChart");
+
+Chart.defaults.global.defaultFontFamily = "Lato";
+Chart.defaults.global.defaultFontSize = 18;
+
+var TCDData = {
+    labels: ["2014", "2015", "2016", "2017", "2018", "2019"],
+    datasets: [
+        {
+            label: "Tasa Basica Pasiva",
+            data: [557.62, 533.31, 548.32, 574.94, 622.99, 610.01]
+        }
+    ]
+};
+
+var chartOption = {
+    legend: {
+        display: true,
+        position: "top",
+        labels: {
+            boxWidth: 80,
+            fontColor: "black"
+        }
+    }
+};
+
+var lineChar = new Chart(TCDCanvas, {
+    type: "line",
+    data: TCDData,
+    options: chartOption
+});
