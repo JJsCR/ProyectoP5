8d5c8319038de9eaca16b2600f176ec8b6acbd15
jjuliangs98cls+¿ <jjuliangs98@gmail.com>
2019-04-23 19:17:37 -0600

    Agregue consulta de foro

---
+++ BancoCentral/BancoCentral/Controllers/ConsultaController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BancoCentral.Models;
+
+namespace BancoCentral.Controllers
+{
+    public class ConsultaController : Controller
+    {
+        private BancoCentralEntities1 db = new BancoCentralEntities1();
+
+        // GET: Consulta
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Consulta.ToListAsync());
+        }
+
+        // GET: Consulta/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = await db.Consulta.FindAsync(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // GET: Consulta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Consulta/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "idConsulta,nombre,consulta1")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Consulta.Add(consulta);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(consulta);
+        }
+
+        // GET: Consulta/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = await db.Consulta.FindAsync(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // POST: Consulta/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "idConsulta,nombre,consulta1")] Consulta consulta)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(consulta).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(consulta);
+        }
+
+        // GET: Consulta/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Consulta consulta = await db.Consulta.FindAsync(id);
+            if (consulta == null)
+            {
+                return HttpNotFound();
+            }
+            return View(consulta);
+        }
+
+        // POST: Consulta/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Consulta consulta = await db.Consulta.FindAsync(id);
+            db.Consulta.Remove(consulta);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
